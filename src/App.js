be3c6d1619148b2b13a3c43596df5e44d329b07e
daghustani <sara.daghustani@outlook.com>
2020-03-09 12:32:29 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import NavbarMain from './Navbar';
 import './App.css';
 import Home from './Home';
-// import { Route, BrowserRouter} from 'react-router-dom';
 import Newpost from './Newpost';
 import  Signup  from './Sign/Signup'
 import {
@@ -11,46 +10,44 @@ import {
 } from 'react-router-dom';
 import Signin from './Sign/Signin';
 
-// function App() {
-  
-//   return (
+function App() {
+  var prePath = "/sara-daghustani/GivingHands";
+  return (
     
-//       <div> 
-  
+      <div> 
+        <NavbarMain />
+        <Router>
 
-//         <NavbarMain />
-//         <BrowserRouter>
-
-//             <Route exact path="/newpost" component={Newpost} />
-//             <Route exact path="/" component={Home} />
-//             <Route path="/signup" component={Signup} />
-//             <Route path="/signin" component={Signin} />
+            <Route path={prePath + "/newpost"} component={Newpost} />
+            <Route exact path={prePath + "/"} component={Home} />
+            <Route path={prePath +"/signup"} component={Signup} />
+            <Route path={prePath +"/signin"} component={Signin} />
  
-//         </BrowserRouter> 
-//       </div>  
-//   );
-// }
-// export default App;
-
-class App extends Component{
-  render(){
-
-
-var prePath = "/GivingHands";
-
-    return(
-      <div>
-      <NavbarMain />
-      <Router>
-        <Route exact path={prePath + "/"} component={Home} />
-        {/* <Route path={prePath + "/home"} component={Home} /> */}
-        <Route path={prePath + "/Newpost"} component={Newpost} />
-        <Route path={prePath + "/Signup"} component={Signup} />
-        <Route path={prePath + "/Signin"} component={Signin} />
-    </Router>
-    </div>
-    );
-  }
+        </Router> 
+      </div>  
+  );
 }
-
 export default App;
+
+// class App extends Component{
+//   render(){
+
+
+// var prePath = "/GivingHands";
+
+//     return(
+//       <div>
+//       <NavbarMain />
+//       <Router>
+//         <Route exact path={prePath + "/"} component={Home} />
+//         {/* <Route path={prePath + "/home"} component={Home} /> */}
+//         <Route path={prePath + "/Newpost"} component={Newpost} />
+//         <Route path={prePath + "/Signup"} component={Signup} />
+//         <Route path={prePath + "/Signin"} component={Signin} />
+//     </Router>
+//     </div>
+//     );
+//   }
+// }
+
+// export default App;
